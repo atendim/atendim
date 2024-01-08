6d15505f3e25eb151f8e624fa4d471ac76fc0797
ItaloMedici <italomedice969@gmail.com>
2024-01-08 16:45:28 -0300

    refactor: use navbar and continer

--- src/app/[locale]/(marketing)/page.tsx
+++ src/app/[locale]/(marketing)/page.tsx
@@ -1,14 +1,15 @@
+import { Container } from '@/components/container';
 import { Button } from '@/components/ui/button';
 import { ChevronRight } from 'lucide-react';
 import { getTranslations } from 'next-intl/server';
 import { Fragment } from 'react';
-import NavBar from './_components/nav-bar';
+import { LandingPageNavBar } from './_components/landing-page-nav-bar';
 
 export default async function MarketingPage() {
   const t = await getTranslations('MarketingPage');
   return (
-    <main className='mx-auto w-full max-w-screen-xl px-5 md:px-20'>
-      <NavBar />
+    <Container>
+      <LandingPageNavBar />
       <div className='flex flex-col content-center items-center gap-10 py-20'>
         <h1 className='text-center text-5xl font-bold tracking-tight text-gray-800 sm:text-6xl'>
           {t('hero.title.prefix')}{' '}
@@ -25,6 +26,6 @@ export default async function MarketingPage() {
           </Fragment>
         </Button>
       </div>
-    </main>
+    </Container>
   );
 }
