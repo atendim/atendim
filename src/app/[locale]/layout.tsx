5e61bf3f9a5d8561d33fa22eae8c46bc090b4cb8
ItaloMedici <italomedice969@gmail.com>
2024-01-09 12:05:29 -0300

    refactor: change to component providers

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,8 @@
+import { Providers } from '@/components/providers';
 import '@/styles/globals.css';
-import { TRPCProvider } from '@/trpc/provider';
 import type { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { Inter } from 'next/font/google';
-import { cookies } from 'next/headers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang={locale}>
       <body className={inter.className}>
-        <TRPCProvider cookies={cookies().toString()}>{children}</TRPCProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
