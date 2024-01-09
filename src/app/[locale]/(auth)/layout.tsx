24f458614084f24fe38fd55846233d9a654f34b4
ItaloMedici <italomedice969@gmail.com>
2024-01-09 17:27:04 -0300

    feat: create auth layout

---
+++ src/app/[locale]/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import { Container } from '@/components/container';
+import { Icons } from '@/components/icons';
+import Link from 'next/link';
+import { PropsWithChildren } from 'react';
+
+function AuthLayout({ children }: PropsWithChildren) {
+  return (
+    <Container>
+      <div className='flex items-center justify-center py-14'>
+        <Link href={'/sign-up'}>
+          <Icons.Logo className='w-48' />
+        </Link>
+      </div>
+      {children}
+    </Container>
+  );
+}
+
+export default AuthLayout;
