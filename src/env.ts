5322b4ba35e01b2b79507417890b7755440317c4
ItaloMedici <italomedice969@gmail.com>
2024-01-10 23:58:05 -0300

    chore: adjust env

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,16 @@
 import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
-  server: {},
+  server: {
+    DATABASE_URL: z.string().url(),
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development')
+  },
   client: {},
-  runtimeEnv: process.env
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL
+  }
 });
