e85111ef3a6e962835b35de30b540346278f91d6
ItaloMedici <italomedice969@gmail.com>
2024-01-12 10:28:55 -0300

    chore: adjust env

--- src/env.ts
+++ src/env.ts
@@ -4,13 +4,17 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(['development', 'test', 'production'])
-      .default('development')
+    AUTH_SECRET: z.string().length(32)
   },
   client: {},
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET
+  },
+  shared: {
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development')
   }
 });
