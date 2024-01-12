1a0d7ddae2a050d86b64eb9aa819aac04eff6e75
ItaloMedici <italomedice969@gmail.com>
2024-01-12 10:27:32 -0300

    feate: create auth route

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,15 +1,36 @@
 import { authCredentialsValidator } from '@/lib/validators/auth-credentials';
+import { HashUtils } from '@/server/utils/hash';
+import { TRPCError } from '@trpc/server';
 import { createTRPCRouter, publicProcedure } from '../trpc';
 
-export const signIn = createTRPCRouter({
-  hello: publicProcedure
+export const auth = createTRPCRouter({
+  signIn: publicProcedure
     .input(authCredentialsValidator)
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       const { email, password } = input;
 
-      const existedUser = ctx.db.user.findUnique({
+      const existedUser = await ctx.db.user.findUnique({
         where: {
           email
+        }
+      });
+
+      if (existedUser) {
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message: ctx.t('Auth.errors.email.emailAlreadyExists')
+        });
+      }
+
+      const hashedPassword = await HashUtils.buildHashedPassword(password);
+
+      const name = email.split('@')[0].replace(/[^a-zA-Z0-9]/g, '');
+
+      return await ctx.db.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          name
         }
       });
     })
